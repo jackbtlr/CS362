f8c5115863094921324a82dd7747a9776aab2310
Jack Butler <jack.butler@dish.com>
2024-10-30 10:47:07 -0600

    Work

--- HW3/classify_bugs.py
+++ HW3/classify_bugs.py
@@ -17,7 +17,7 @@ def analyze_bug(num):
     res = ''
     if num[0] == '4':
         res += 'visa,'
-    elif num[0:2] == 34 or num[0:2] == 37:
+    elif num[0:2] == '34' or num[0:2] == '37':
         res += 'amex,'
     elif 51 <= int(num[0:2]) <= 55 or 2221 <= int(num[0:4]) <= 2720:
         res += 'mastercard,'
@@ -46,5 +46,23 @@ if __name__ == '__main__':
             else:
                 bugs[current].append(t)
     cases = classify_bugs(bugs)
-    print(json.dumps(cases, indent = 4))
+    #print(json.dumps(cases, indent = 4, sort_keys=True))
+    bug7_list = []
+    for set in cases['Bug 7:']:
+        bug7_list += cases['Bug 7:'][set]
+    exam = {}
+    for num in bug7_list:
+        exam[num] = {}
+        for c in num:
+            exam[num][c] = exam[num].get(c, 0) + 1
+    for i in range(10):
+        c = str(i)
+        match = True
+        count = exam['2116622138520802'].get(c, 0)
+        for num in exam:
+            if exam[num].get(c, 0) != count:
+                match = False
+        if match:
+            print(f'Count of {i}: {count}')
+    print(json.dumps(exam, indent = 4, sort_keys=True))
         
